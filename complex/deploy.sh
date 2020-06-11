f3dd8f63895cb2278a85ecb8553964b9c3905052
Alex G <alex.gure@yahoo.com>
2020-06-11 15:39:02 +0000

    update

--- complex/deploy.sh
+++ complex/deploy.sh
@@ -1,6 +1,6 @@
-docker build -t alex3dd/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t alex3dd/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t alex3dd/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t alex3dd/multi-client:latest -t alex3dd/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t alex3dd/multi-server:latest -t alex3dd/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t alex3dd/multi-worker:latest -t alex3dd/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push alex3dd/multi-client:latest
 docker push alex3dd/multi-server:latest
